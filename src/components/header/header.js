98e264775a8f025a4d1c5d3f99800eb83afb441e
ieun32 <ueunviss@gmail.com>
2024-05-18 11:54:40 +0900

    ✅ head 컴포넌트

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import * as styles from "./header.module.css";
+
+const header = `
+<header class="${styles.header}">
+  <button data-navigate="main">maelong</button>
+</header>
+`;
+
+export default header;
